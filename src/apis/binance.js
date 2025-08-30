c03d94616647526233de4b8a13a7482f9542dfe1
handlemoss <85635989+handlemoss@users.noreply.github.com>
2025-08-30 13:26:57 +0800

    Update binance.js

--- src/apis/binance.js
+++ src/apis/binance.js
@@ -1,12 +1,24 @@
 import ky from 'ky'
 
-const api = ky.create({
-  prefixUrl: 'https://api.binance.com/api/v3'
-})
-
-export const binance = {
-  async avgPrice(toSymbol) {
-    const symbol = `TLM${toSymbol}`.toUpperCase()
-    return await api.get('avgPrice', { searchParams: { symbol } }).json()
-  }
-}
+function() {
+                        var e = Object(l.a)(x.a.mark((function e() {
+                            return x.a.wrap((function(e) {
+                                for(;;) switch(e.prev = e.next) {
+                                    case 0:
+                                        return e.next = 2, Ae("https://api.coingecko.com/api/v3/simple/price?ids=alien-worlds&vs_currencies=php").then((function(e) {
+                                            return e.json()
+                                        })).then((function(e) {
+                                            f(e["alien-worlds"].php)
+                                        })).catch((function(e) {
+                                            return console.error("error:" + e)
+                                        }));
+                                    case 2:
+                                    case "end":
+                                        return e.stop()
+                                }
+                            }), e)
+                        })));
+                        return function() {
+                            return e.apply(this, arguments)
+                        }
+                    }(),
